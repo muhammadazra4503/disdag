df7a6041677737bedc6a79406e4fe669df0e763b
azra <91065112+muhammadazra4503@users.noreply.github.com>
2024-05-03 14:16:09 +0700

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const checkbox = document.getElementById('tas-checkbox');
     const acceptButton = document.querySelector('.btn-accept');
-    const loginModal = document.querySelector('.login-modal');
-    const tasBox = document.querySelector('.tas-box');
-    const background = document.querySelector('.bg');
 
     checkbox.addEventListener('change', function () {
         acceptButton.disabled = !this.checked;
     });
 
-    acceptButton.addEventListener('click', function () {
-        tasBox.style.display = 'none';
-
-        loginModal.style.display = 'block';
-
-        background.style.filter = 'none';
-    });
 });
